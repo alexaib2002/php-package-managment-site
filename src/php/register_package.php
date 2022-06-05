0ebbb1e365e09b7e99c12ed83be2edd8a17ce081
alexaib <alexaib@outlook.es>
2022-06-05 23:49:28 +0200

    added form element names for $_POST

--- src/php/register_package.php
+++ src/php/register_package.php
@@ -1,5 +1,9 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    // TODO notify user
+}
+
 function generateActorCard(string $form_id, string $title): string {
     return <<<HTML
 <div class="card-header"> <!--TODO add background image-->
@@ -9,19 +13,19 @@ function generateActorCard(string $form_id, string $title): string {
     <li class="list-group-item">
         <div class="row g-2">
             <div class="col form-floating">
-                <input type="text" aria-label="First name" class="form-control" value="" id="input" .$form_id."FirstName">
-                <label for="input" .$form_id."FirstName">First name</label>
+                <input type="text" aria-label="First name" class="form-control" value="" id="input$form_id.FirstName" name="input$form_id.FirstName">
+                <label for="input$form_id.FirstName">First name</label>
             </div>
             <div class="col form-floating">
-                <input type="text" aria-label="Last name" class="form-control" value="" id="input" .$form_id."LastName">
-                <label for="input" .$form_id."LastName">Last name</label>
+                <input type="text" aria-label="Last name" class="form-control" value="" id="input$form_id.LastName">
+                <label for="input$form_id.LastName">Last name</label>
             </div>
         </div>
         <div class="input-group my-2">
-            <input type="text" aria-label="E-Mail username" class="form-control" placeholder="$form_id's mail username" id="input" .$form_id."EmailUser">
+            <input type="text" aria-label="E-Mail username" class="form-control" placeholder="$form_id's mail username" id="input$form_id.EmailUser">
             <span class="input-group-text">@</span>
-            <input type="text" aria-label="E-Mail domain" class="form-control" placeholder="$form_id's email domain" id="input" .$form_id."EmailDomain">
-            <select class="form-select" id="input" .$form_id."EmailDomainExtension">
+            <input type="text" aria-label="E-Mail domain" class="form-control" placeholder="$form_id's email domain" id="input$form_id.EmailDomain">
+            <select class="form-select" id="input$form_id.EmailDomainExtension">
                 <option value="com">.com</option>
                 <option value="es">.es</option>
                 <option value="net">.net</option>
@@ -33,7 +37,7 @@ function generateActorCard(string $form_id, string $title): string {
         </div>
         <div class="row my-2 g-2">
             <div class="col form-floating">
-                <select class="form-floating form-select" id="input" .$form_id."PhoneExtension">
+                <select class="form-floating form-select" id="input$form_id.PhoneExtension">
                     <option value="+34">+34 (Spain)</option>
                     <option value="+1">+1 (USA)</option>
                     <option value="+44">+44 (UK)</option>
@@ -42,26 +46,33 @@ function generateActorCard(string $form_id, string $title): string {
                     <option value="+39">+39 (Italy)</option>
                     <option value="+41">+41 (Switzerland)</option>
                 </select>
-                <label class="mx-2" for="input" .$form_id."PhoneExtension">Number extension:</label>
+                <label class="mx-2" for="input$form_id.PhoneExtension">Number extension:</label>
             </div>
             <div class="col form-floating">
-                <input type="tel" class="form-control" id="input" .$form_id."PhoneNumber" value="">
-                <label class="mx-2" for="input" .$form_id."PhoneNumber">Phone number</label>
+                <input type="tel" class="form-control" id="input$form_id.PhoneNumber" value="">
+                <label class="mx-2" for="input$form_id.PhoneNumber">Phone number</label>
             </div>
         </div>
     </li>
     <li class="list-group-item">
         <div class="input-group">
             <span class="input-group-text">Address</span>
-            <input type="text" class="form-control" placeholder="Street">
-            <input type="text" class="form-control" placeholder="City">
-            <input type="text" class="form-control" placeholder="State">
-            <input type="text" class="form-control" placeholder="Zip">
+            <input type="text" class="form-control" placeholder="Street" name="input$form_id.Address.Street" id="input$form_id.Address.Street">
+            <input type="text" class="form-control" placeholder="City" name="input$form_id.Address.City" id="input$form_id.Address.City">
+            <input type="text" class="form-control" placeholder="State" name="input$form_id.Address.State" id="input$form_id.Address.State">
+            <input type="text" class="form-control" placeholder="Zip" name="input$form_id.Address.Zip" id="input$form_id.Address.Zip">
         </div>
     </li>
     <li class="list-group-item">
-        <div class="input-group">
-
+        <div class="container row">
+            <div class="form-check form-switch col">
+              <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" name="input$form_id.FlexSwitchCheckDefault">
+              <label class="form-check-label" for="flexSwitchCheckDefault">Send package status updates</label>
+            </div>
+            <div class="form-check form-switch col">
+              <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" name="input$form_id.FlexSwitchCheckDefault">
+              <label class="form-check-label" for="flexSwitchCheckDefault">Send info related to promotions</label>
+            </div>
         </div>
     </li>
 </ul>
@@ -127,14 +138,14 @@ HTML;
             <h1>Register new delivery</h1>
             <p>Please, complete the form below to add a new package to the system</p>
             <hr>
-            <form>
+            <form action="register_package.php" method="post" >
                 <!-- Sender Card -->
                 <div class="card m-3 w-75 mx-auto" id="sender_details">
                     <?php echo generateActorCard("Sender", "Who's sending the package?"); ?>
                 </div>
                 <!-- Receiver Card -->
                 <div class="card m-3 w-75 mx-auto" id="receiver_details">
-                    <?php echo generateActorCard("receiver", "Where do we deliver the package?"); ?>
+                    <?php echo generateActorCard("Receiver", "Where do we deliver the package?"); ?>
                 </div>
                 <!-- Package Card -->
                 <div class="card m-3 w-75 mx-auto" id="package_details">
@@ -146,16 +157,16 @@ HTML;
                             <div class="row">
                                 <div class="input-group col m-1 w-50">
                                     <span class="input-group-text w-25">Weight</span>
-                                    <input type="text" aria-label="Weight" class="form-control" placeholder="0" id="inputWeight">
-                                    <select id="inputWeightUnit" class="form-select">
+                                    <input type="text" aria-label="Weight" class="form-control" placeholder="0" id="inputWeight" name="inputWeight">
+                                    <select id="inputWeightUnit" class="form-select" name="inputWeightUnit">
                                         <option value="kg">kg</option>
                                         <option value="lb">lb</option>
                                     </select>
                                 </div>
                                 <div class="input-group col m-1 w-50">
                                     <span class="input-group-text w-25">Length</span>
-                                    <input type="text" aria-label="Length" class="form-control" placeholder="0" id="inputLength">
-                                    <select id="inputLengthUnit" class="form-select">
+                                    <input type="text" aria-label="Length" class="form-control" placeholder="0" id="inputLength" name="inputLength">
+                                    <select id="inputLengthUnit" class="form-select" name="inputLengthUnit">
                                         <option value="cm">cm</option>
                                         <option value="in">in</option>
                                     </select>
@@ -164,16 +175,16 @@ HTML;
                             <div class="row">
                                 <div class="input-group col m-1 w-25">
                                     <span class="input-group-text w-25">Width</span>
-                                    <input type="text" aria-label="Width" class="form-control" placeholder="Width" id="inputWidth">
-                                    <select id="inputWidthUnit" class="form-select">
+                                    <input type="text" aria-label="Width" class="form-control" placeholder="Width" id="inputWidth" name="inputWidth">
+                                    <select id="inputWidthUnit" class="form-select" name="inputWidthUnit">
                                         <option value="cm">cm</option>
                                         <option value="in">in</option>
                                     </select>
                                 </div>
                                 <div class="input-group col m-1 w-25">
                                     <span class="input-group-text w-25">Height</span>
-                                    <input type="text" aria-label="Height" class="form-control" placeholder="Height" id="inputHeight">
-                                    <select id="inputHeightUnit" class="form-select">
+                                    <input type="text" aria-label="Height" class="form-control" placeholder="Height" id="inputHeight" name="inputHeight">
+                                    <select id="inputHeightUnit" class="form-select" name="inputHeightUnit">
                                         <option value="cm">cm</option>
                                         <option value="in">in</option>
                                     </select>
@@ -185,16 +196,16 @@ HTML;
                                 <legend class="col-form-label col-sm-2 pt-0">Delivery type</legend>
                                 <div class="col-sm-10">
                                     <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="standard" checked>
-                                        <label class="form-check-label" for="gridRadios1">Standard</label>
+                                        <input class="form-check-input" type="radio" name="deliveryTypeRadios" id="standardRadio" value="standard" checked>
+                                        <label class="form-check-label" for="standardRadio">Standard</label>
                                     </div>
                                     <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="express">
-                                        <label class="form-check-label" for="gridRadios2">Express</label>
+                                        <input class="form-check-input" type="radio" name="deliveryTypeRadios" id="expressRadio" value="express">
+                                        <label class="form-check-label" for="expressRadio">Express</label>
                                     </div>
                                     <div class="form-check disabled">
-                                        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="overnight">
-                                        <label class="form-check-label" for="gridRadios3">Overnight</label>
+                                        <input class="form-check-input" type="radio" name="deliveryTypeRadios" id="overnightRadio" value="overnight">
+                                        <label class="form-check-label" for="overnightRadio">Overnight</label>
                                     </div>
                                 </div>
                             </fieldset>
